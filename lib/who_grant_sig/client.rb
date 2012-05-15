c3112eef41fa43ce7bdd5db4e337d3338abe2832
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-05-14 23:07:20 -0700

    make a basic client

--- lib/who_grant_sig/client.rb
+++ lib/who_grant_sig/client.rb
@@ -1,4 +1,16 @@
 module WhoGrantSig
   class Client
+
+    def get(requestor, url)
+      conn = Faraday.new do |builder|
+        builder.use Faraday::Request::UrlEncoded  # convert request params as "www-form-urlencoded"
+        builder.use Faraday::Response::Logger     # log the request to STDOUT
+        builder.use Faraday::Adapter::NetHttp     # make http requests with Net::HTTP
+
+        builder.user Faraday::Request::WhoGrantSig(requestor)
+      end
+
+      conn.get(url)
+    end
   end
 end
