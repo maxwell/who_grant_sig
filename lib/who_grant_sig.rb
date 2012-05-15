cb536d260c9634f579a2d4632cbd7d569d892d0c
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-05-14 23:10:19 -0700

    add client to gem

--- lib/who_grant_sig.rb
+++ lib/who_grant_sig.rb
@@ -5,6 +5,7 @@ require "base64"
 require 'who_grant_sig/header'
 require 'who_grant_sig/validator'
 require 'who_grant_sig/who_grant_sig_middleware'
+require 'who_grant_sig/client'
 
 module WhoGrantSig
   TIME_THRESHOLD = 60 * 5 # 5 minutes
